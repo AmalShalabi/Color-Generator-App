4691c19b7d34ac5dbd00867b88bb65b76af679ee
unknown <mouni.shalaby@gmail.com>
2023-03-05 15:44:29 +0200

    copy to clipboard

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#016064").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
       setList(colors);
+      setError(false);
     } catch (error) {
       setError(true);
       console.log(error);
@@ -37,9 +38,16 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        {list.map((color,index)=>{
-          console.log(color)
-          return <SingleColor key={index} {...color} index={index} hexColor={color.hex}/>
+        {list.map((color, index) => {
+          console.log(color);
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
         })}
       </section>
     </>
